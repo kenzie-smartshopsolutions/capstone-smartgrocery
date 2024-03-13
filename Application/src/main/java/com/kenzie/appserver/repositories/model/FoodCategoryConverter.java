590d33d7da064d510a767437df80ad25832b4380
jvubeepboop <john.vu@mykenzie.snhu.edu>
2024-03-13 12:29:37 -0700

    Pantry Management updated & implementation of csv/json convertter

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/FoodCategoryConverter.java
@@ -0,0 +1,34 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class FoodCategoryConverter {
+    public static Map<String, String> convertCsvToCategoryMap() throws IOException {
+        File file = new File("/food_category.csv");
+        CsvSchema bootstrap = CsvSchema.emptySchema().withHeader();
+        CsvMapper csvMapper = new CsvMapper();
+
+        Map<String, String> foodCategories = new HashMap<>();
+        try (MappingIterator<Map<?, ?>> mappingIterator = csvMapper.readerFor(Map.class).with(bootstrap).readValues(file)) {
+            for (Map<?, ?> row : mappingIterator.readAll()) {
+
+                // Uses the "id" column
+                String id = (String) row.get("id");
+
+                // Uses "description column
+                String description = (String) row.get("description");
+
+                foodCategories.put(id, description);
+            }
+        }
+        return foodCategories;
+    }
+
+
+}
