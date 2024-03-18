c587fc6bc055f704f7d464624d995d7a3b95ac6d
jvubeepboop <john.vu@mykenzie.snhu.edu>
2024-03-18 04:37:10 -0700

    @RequestMapping quick fix test

--- Application/src/main/java/com/kenzie/appserver/controller/UserController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("User")
 public class UserController {
 
     @Autowired
