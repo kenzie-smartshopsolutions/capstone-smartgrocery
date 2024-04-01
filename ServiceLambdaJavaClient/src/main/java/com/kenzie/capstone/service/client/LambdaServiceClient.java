f01cbf08c99dd7ad259bca25e895763db6291ff4
jvubeepboop <john.vu@mykenzie.snhu.edu>
2024-04-01 11:00:48 -0700

    update

--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/LambdaServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/LambdaServiceClient.java
@@ -1,5 +1,6 @@
 package com.kenzie.capstone.service.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kenzie.capstone.service.model.ExampleData;
 import com.kenzie.capstone.service.model.UserData;
@@ -10,8 +11,8 @@ public class LambdaServiceClient {
     private static final String GET_EXAMPLE_ENDPOINT = "example/{id}";
     private static final String SET_EXAMPLE_ENDPOINT = "example";
 
-    private static final String GET_USER_ENDPOINT = "User/{userId}";
-    private static final String SET_USER_ENDPOINT = "User";
+    private static final String GET_USER_ENDPOINT = "User/register/userId/{userId}";
+    private static final String SET_USER_ENDPOINT = "User/register";
 
     private ObjectMapper mapper;
 
@@ -45,24 +46,38 @@ public class LambdaServiceClient {
 
     public UserData getUserData(String userId) {
         EndpointUtility endpointUtility = new EndpointUtility();
-        String response = endpointUtility.getEndpoint(GET_EXAMPLE_ENDPOINT.replace("{userId}", userId));
+        String response = endpointUtility.getEndpoint(GET_USER_ENDPOINT.replace("{userId}", userId));
         UserData userData;
         try {
             userData = mapper.readValue(response, UserData.class);
+        } catch (JsonProcessingException e) {
+        // If there is an issue with deserializing the response, handle it appropriately
+            throw new ApiGatewayException("Unable to deserialize JSON response: " + e.getMessage());
+        } catch (ApiGatewayException e) {
+        // If an ApiGatewayException is thrown, re-throw it
+            throw e;
         } catch (Exception e) {
-            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        // For other exceptions, handle them and wrap them in an ApiGatewayException
+            throw new ApiGatewayException("An error occurred while processing the response: " + e.getMessage());
         }
         return userData;
     }
 
     public UserData setUserData(String data) {
         EndpointUtility endpointUtility = new EndpointUtility();
-        String response = endpointUtility.postEndpoint(SET_EXAMPLE_ENDPOINT, data);
+        String response = endpointUtility.postEndpoint(SET_USER_ENDPOINT, data);
         UserData userData;
         try {
             userData = mapper.readValue(response, UserData.class);
+        } catch (JsonProcessingException e) {
+        // If there is an issue with deserializing the response, handle it appropriately
+            throw new ApiGatewayException("Unable to deserialize JSON response: " + e.getMessage());
+        } catch (ApiGatewayException e) {
+        // If an ApiGatewayException is thrown, re-throw it
+            throw e;
         } catch (Exception e) {
-            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        // For other exceptions, handle them and wrap them in an ApiGatewayException
+            throw new ApiGatewayException("An error occurred while processing the response: " + e.getMessage());
         }
         return userData;
     }
