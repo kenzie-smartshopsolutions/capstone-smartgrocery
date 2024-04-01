c763a3490350d83683fe726240953493decc66b0
John <131907851+jvubeepboop@users.noreply.github.com>
2024-04-01 12:56:08 -0700

    Update ExampleControllerTest.java

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/ExampleControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/ExampleControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@IntegrationTest
+// @IntegrationTest
 class ExampleControllerTest {
    @Autowired
    private MockMvc mvc;
