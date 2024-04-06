6647753b2b5f0c41cf1f7202321840595e5ba768
jvubeepboop <john.vu@mykenzie.snhu.edu>
2024-04-06 14:32:50 -0700

    updated createPipeline.sh & deployDev.sh to confirm & wait for stack completion

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/PantryLambdaService.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/PantryLambdaService.java
@@ -1,17 +1,12 @@
 package com.kenzie.capstone.service;
 
-import com.kenzie.capstone.service.dao.ExampleDao;
 import com.kenzie.capstone.service.dao.PantryDao;
-import com.kenzie.capstone.service.dao.UserDao;
-import com.kenzie.capstone.service.model.*;
+import com.kenzie.capstone.service.model.PantryData;
+import com.kenzie.capstone.service.model.PantryRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.util.List;
-import java.util.UUID;
-
-//import static com.google.common.util.concurrent.SerializingExecutor.log;
 
 public class PantryLambdaService {
     private PantryDao pantryDao;
