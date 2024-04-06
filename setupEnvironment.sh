59119ac1432f280499a040c006e71ccec7dc9a23
John <131907851+jvubeepboop@users.noreply.github.com>
2024-04-06 15:11:19 -0700

    Update setupEnvironment.sh

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -19,7 +19,7 @@ export GITHUB_GROUP_NAME=smartshopsolutions
 # For Windows
 # Copy and paste ALL of the properties below into your .bashrc file in your home directory
 #export JAVA_HOME=c/Users/12146/.jdks/corretto-16.0.2
-#export GITHUB_TOKEN=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
+export GITHUB_TOKEN=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
 # In IntelliJ Terminal
 # Type source ./setupEnvironment.sh
 
